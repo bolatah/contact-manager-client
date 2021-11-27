0730febccc1c40c406ec7f7339cf89aaa923ca59
bolatah <bolatah@outlook.com>
2021-11-27 11:54:31 +0100

    added a model to draw the data

---
+++ src/routes/getContacts.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Name', width: 130 },
+  { field: 'email', headerName: 'Email', width: 130 },
+  
+  {
+    field: 'name',
+    headerName: 'Name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.getValue(params.id, 'firstName') || ''} ${
+        params.getValue(params.id, 'lastName') || ''
+      }`,
+  },
+];
+
+const rows = [
+  { id: 1, name: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, name: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, name: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, name: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, name: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, name: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, name: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, name: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, name: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function DataTable() {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
+}
